8a4753418e31636dfd04dae031aeeba292446490
Carlos Cubas <carlos.cubas@gmail.com>
2024-07-01 19:11:07 -0300

    remove unsed files

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
    alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
+
+buildscript {
+    extra.apply {
+        set("room_version", "2.6.0")
+    }
+}
