9cceafc0c1f3c70a33f01d16464caed8d93dc7ae
Carlos Cubas <carlos.cubas@gmail.com>
2024-09-12 19:11:34 -0300

    remove some unused files

--- app/src/main/java/com/example/agenda/Lista.kt
+++ app/src/main/java/com/example/agenda/Lista.kt
@@ -17,48 +17,10 @@ class Lista : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity);
 
-        val myToolbar: Toolbar = findViewById(R.id.toolbar)
-        myToolbar.setTitleTextColor(Color.WHITE)
-        setSupportActionBar(myToolbar)
-
-
         val contatos = arrayOf("Maria", "José", "Carlos")
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contatos)
 
         val lista: ListView = findViewById(R.id.lista)
         lista.setAdapter(adapter);
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.novo -> {
-                val intent = Intent(this, ContatoActivity::class.java)
-                startActivity(intent)
-                return false
-            }
-
-            R.id.sincronizar -> {
-                Toast.makeText(this, "Enviar", Toast.LENGTH_LONG).show()
-                return false
-            }
-
-            R.id.receber -> {
-                Toast.makeText(this, "Receber", Toast.LENGTH_LONG).show()
-                return false
-            }
-
-            R.id.mapa -> {
-                Toast.makeText(this, "Mapa", Toast.LENGTH_LONG).show()
-                return false
-            }
-
-            else -> return super.onOptionsItemSelected(item)
-        }
     }
 }
